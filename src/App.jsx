a7c376a869664a2f37d0c8b18e38f4cdd5510a64
ING Jeferson Vergara <jf.vergara12@gmail.com>
2022-12-13 21:30:50 -0500

    creacion de hooks separando bd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import './App.css'
 import FormUser from './components/FormUser'
 import UserCard from './components/UserCard'
+import useCrud from './hooks/useCrud'
+
 
 function App() {
- const [users, setusers] = useState()
+  const {users,getAllUser, createNewUser, updateUserById,deleteUserById}= useCrud()
+ 
  const [updateInfo, setupdateInfo] = useState()
 
 useEffect(() => {
@@ -14,38 +16,6 @@ useEffect(() => {
  }, [])
 
  
- const getAllUser = () => {
-  const URL = 'https://users-crud.academlo.tech/users/'
-  axios.get(URL)
-  .then(res=> setusers(res.data))
-  .catch(err=> console.log(err))
- }
-
- 
-
- const createNewUser =data=>{
-   const URL = 'https://users-crud.academlo.tech/users/'
-   axios.post(URL, data)
-    .then(()=> getAllUser())
-    .catch(err=> console.log(err))
-}
- //console.log(updateInfo)
-
- const deleteUserById =(id)=>{
-      const URL = `https://users-crud.academlo.tech/users/${id}/` 
-      axios.delete(URL)
-      .then(() => getAllUser())
-      .catch(err=> console.log(err))
-}
- 
-  const updateUserById =(id , data) =>{
-    URL = `https://users-crud.academlo.tech/users/${id}/` 
-    axios.put(URL,data)
-    .then(()=> getAllUser())
-    .catch(err=> console.log(err))
-  }
-
-
   return (
     <div className="App">
 
