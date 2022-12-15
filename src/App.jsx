41295ffe89acf25b6ec5c4de30eacd08c2af5521
ING Jeferson Vergara <jf.vergara12@gmail.com>
2022-12-14 23:22:26 -0500

    finish backend and frontend

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import useCrud from './hooks/useCrud'
 
 
 function App() {
+const [closeForm, setcloseForm] = useState(true)
+
   const {users,getAllUser, createNewUser, updateUserById,deleteUserById}= useCrud()
  
  const [updateInfo, setupdateInfo] = useState()
@@ -20,13 +22,17 @@ useEffect(() => {
     <div className="App">
 
       <h1>User</h1>
-      <button> Open Form</button>
-      <FormUser
+      <button  onClick={()=> setcloseForm(false)} className='App__btn'> Open Form</button>
+
+   <div className={`form-container ${closeForm && 'close__form'}`}>
+   <FormUser
       createNewUser={createNewUser}
       updateInfo ={updateInfo}
       updateUserById ={updateUserById}
       setupdateInfo ={setupdateInfo}
+      setcloseForm={setcloseForm}
       />
+   </div>
       <div className="user-container">
       {
         users?.map(user => (
--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import './styles/formUser.css'
 
-const FormUser = ({createNewUser, updateInfo,updateUserById,setupdateInfo}) => {
+const FormUser = ({createNewUser, updateInfo,updateUserById,setupdateInfo,setcloseForm}) => {
     //console.log(updateInfo)
 
     useEffect(() => {
@@ -36,29 +37,30 @@ const FormUser = ({createNewUser, updateInfo,updateUserById,setupdateInfo}) => {
   }
 
   return (
-    <form onSubmit={handleSubmit(submit)}>
-      <h2>{updateInfo? 'Update User': "Create User"}</h2>
-      <div>
-        <label htmlFor="email">EMAIL</label>
-        <input type="email" id="email" {...register("email")} />
+    <form className="form" onSubmit={handleSubmit(submit)}>
+      <div  onClick={()=> setcloseForm(true)} className="form__x">❌</div>
+      <h2 className="form__title">{updateInfo? 'Update User': "Create User"}</h2>
+      <div className="form__div">
+        <label  className="form__label" htmlFor="email">EMAIL</label>
+        <input className="form__input" type="email" id="email" {...register("email")} />
       </div>
-      <div>
-        <label htmlFor="email">PASSWORD</label>
-        <input type="password" id="password" {...register("password")} />
+      <div className="form__div">
+        <label className="form__label" htmlFor="email">PASSWORD</label>
+        <input className="form__input" type="password" id="password" {...register("password")} />
       </div>
-      <div>
-        <label htmlFor="email">FIRST NAME</label>
-        <input type="text" id="first_name" {...register("first_name")}/>
+      <div className="form__div">
+        <label className="form__label" htmlFor="email">FIRST NAME</label>
+        <input className="form__input" type="text" id="first_name" {...register("first_name")}/>
       </div>
-      <div>
-        <label htmlFor="email">LAST NAME</label>
-        <input type="text" id="last_name"{...register("last_name")} />
+      <div className="form__div">
+        <label className="form__label" htmlFor="email">LAST NAME</label>
+        <input className="form__input" type="text" id="last_name"{...register("last_name")} />
       </div>
-      <div>
-        <label htmlFor="email">BIRTHDAY</label>
-        <input type="date" id="birthday"{...register("birthday")} />
+      <div className="form__div">
+        <label  className="form__label"htmlFor="email">BIRTHDAY</label>
+        <input className="form__input" type="date" id="birthday"{...register("birthday")} />
       </div>
-      <button>Submit</button>
+      <button className="form__btn">Submit</button>
     </form>
   );
 };
